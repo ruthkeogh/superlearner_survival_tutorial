c8650701a25b255bed1b8275cab10bb2353d46a3
Ruth Keogh <ruth.keogh@lshtm.ac.uk>
2025-07-23 13:58:38 +0100

    Add files via upload

--- SL_discrete_byhand.R
+++ SL_discrete_byhand.R
@@ -18,27 +18,25 @@ dta_train$cv.group<-cut(runif(n,0,n.fold),breaks=seq(0,n.fold,1),include.lowest 
 
 #---
 #put data into counting process format using 30 day periods
-dta.split<-survSplit(Surv(time,status)~.,data=dta_train,cut = seq(0,max(dta_train$time),0.1),start="tstart",end="tstop",event="status")
+dta_train_split<-survSplit(Surv(time,status)~.,data=dta_train,cut = seq(0,max(dta_train$time),0.1),start="tstart",end="tstop",event="status")
 
 #generate time^2 and time^3 variables to be used as covariates in the SL
-dta.split$tstart2<-dta.split$tstart^2
-dta.split$tstart3<-dta.split$tstart^3
+dta_train_split$tstart2<-dta_train_split$tstart^2
+dta_train_split$tstart3<-dta_train_split$tstart^3
 
 #---
 #data that we will use to predict hazards at all times
 times<-data.frame(tstart=seq(0,max(dta_train$time),0.1))
-dta.haz<-merge(dta_train,times)
-dta.haz<-dta.haz[order(dta.haz$pid,dta.haz$tstart),]
-dta.haz$tstart2<-dta.haz$tstart^2
-dta.haz$tstart3<-dta.haz$tstart^3
+dta_train_haz<-merge(dta_train,times)
+dta_train_haz<-dta_train_haz[order(dta_train_haz$pid,dta_train_haz$tstart),]
+dta_train_haz$tstart2<-dta_train_haz$tstart^2
+dta_train_haz$tstart3<-dta_train_haz$tstart^3
 
 #---
-#storing predicted hazards from each method
+#storing predicted hazards and survival probabilities (cross-fitted) from each method
 n.methods=4
-haz.pred<-matrix(nrow=dim(dta.split)[1],ncol=n.methods)
-haz.pred.all<-matrix(nrow=dim(dta.haz)[1],ncol=n.methods)
-
-#storing predicted survival probabilities from each method
+haz.pred<-matrix(nrow=dim(dta_train_split)[1],ncol=n.methods)
+haz.pred.all<-matrix(nrow=dim(dta_train_haz)[1],ncol=n.methods)
 surv.pred<-matrix(nrow=n,ncol=n.methods)
 
 #storing expected loss from each method and each fold
@@ -46,17 +44,22 @@ loss.L2<-matrix(nrow=n.fold,ncol=n.methods)
 loss.loglik<-matrix(nrow=n.fold,ncol=n.methods)
 loss.ipcw<-matrix(nrow=n.fold,ncol=n.methods)
 
+#storing censoring weights
+# cens.wt<-rep(NA,n.fold)
+cens.wt<-rep(NA,n)
+
 #---
 #start of cross-validation loop
 for(i in 1:n.fold){
   print(i)
   
-  train<-dta.split[!dta.split$cv.group==i,] #data used to fit models
-  test<-dta.haz[dta.haz$cv.group==i,] #test data - note this has 1 row for each time for each person (including times after the observed event/cens time)
-  test.rows<-which(dta.haz$cv.group==i) #used for storing results
+  cvtrain<-dta_train_split[!dta_train_split$cv.group==i,] #data used to fit models
+  cvtest<-dta_train_haz[dta_train_haz$cv.group==i,] #test data - note this has 1 row for each time for each person (including times after the observed event/cens time)
+  cvtest.rows<-which(dta_train_haz$cv.group==i) #used for storing results
   
-  test.1row<-dta_train[dta_train$cv.group==i,]#test data with 1 row per person, used for estimating expected loss
-  test.split<-dta.split[dta.split$cv.group==i,] #test data with multiple rows per person, used for estimating expected loss
+  cvtest.1row<-dta_train[dta_train$cv.group==i,]#test data with 1 row per person, used for estimating expected loss
+  cvtest.split<-dta_train_split[dta_train_split$cv.group==i,] #test data with multiple rows per person, used for estimating expected loss
+  cvtest.rows.1row<-which(dta_train$cv.group==i) #used for storing results
   
   #-------------------------------------------------------
   #Fit candidate models and obtain cross-fitted estimates
@@ -65,36 +68,36 @@ for(i in 1:n.fold){
   
   #---
   #method 1: glm with no covariates
-  method.glm0<-glm(status~1,data=train,family = "binomial")
-  haz.pred.all[test.rows,1]<-predict(method.glm0,newdata=test,type="response")
-  haz.pred[dta.split$cv.group==i,1]<-haz.pred.all[test.rows,1][test$tstart<test$time]
-  surv.pred[dta_train$cv.group==i,1]<-ave(1-haz.pred.all[test.rows,1],test$pid,FUN=cumprod)[test$tstart==10]
+  method.glm0<-glm(status~1,data=cvtrain,family = "binomial")
+  haz.pred.all[cvtest.rows,1]<-predict(method.glm0,newdata=cvtest,type="response")
+  haz.pred[dta_train_split$cv.group==i,1]<-haz.pred.all[cvtest.rows,1][cvtest$tstart<cvtest$time]
+  surv.pred[dta_train$cv.group==i,1]<-ave(1-haz.pred.all[cvtest.rows,1],cvtest$pid,FUN=cumprod)[cvtest$tstart==10]
   
   #---
   #method 2: glm including time plus covariates
   method.glm1<-glm(status~tstart+tstart2+tstart3+year1+year2+age+meno+size1+size2+grade+nodes+pgr+er+hormon+chemo,
-                   data=train,family = "binomial")
-  haz.pred.all[test.rows,2]<-predict(method.glm1,newdata=test,type="response")
-  haz.pred[dta.split$cv.group==i,2]<-haz.pred.all[test.rows,2][test$tstart<test$time]
-  surv.pred[dta_train$cv.group==i,2]<-ave(1-haz.pred.all[test.rows,2],test$pid,FUN=cumprod)[test$tstart==10]
+                   data=cvtrain,family = "binomial")
+  haz.pred.all[cvtest.rows,2]<-predict(method.glm1,newdata=cvtest,type="response")
+  haz.pred[dta_train_split$cv.group==i,2]<-haz.pred.all[cvtest.rows,2][cvtest$tstart<cvtest$time]
+  surv.pred[dta_train$cv.group==i,2]<-ave(1-haz.pred.all[cvtest.rows,2],cvtest$pid,FUN=cumprod)[cvtest$tstart==10]
 
   #---
   #method 3: lasso
-  xmat.train<-model.matrix(method.glm1)[,-1]
+  xmat.cvtrain<-model.matrix(method.glm1)[,-1]
   test$status=99#just a trick so that we can use model.matrix to get xmat.test
-  xmat.test<-model.matrix(method.glm1,data=test)[,-1]
-  method.lasso <- cv.glmnet(x=xmat.train,y=train$status,family = "binomial",alpha=1)
-  haz.pred.all[test.rows,3]<-predict(method.lasso, newx = xmat.test, s = "lambda.min",type="response")
-  haz.pred[dta.split$cv.group==i,3]<-haz.pred.all[test.rows,3][test$tstart<test$time]
-  surv.pred[dta_train$cv.group==i,3]<-ave(1-haz.pred.all[test.rows,3],test$pid,FUN=cumprod)[test$tstart==10]
+  xmat.cvtest<-model.matrix(method.glm1,data=cvtest)[,-1]
+  method.lasso <- cv.glmnet(x=xmat.cvtrain,y=cvtrain$status,family = "binomial",alpha=1)
+  haz.pred.all[cvtest.rows,3]<-predict(method.lasso, newx = xmat.cvtest, s = "lambda.min",type="response")
+  haz.pred[dta_train_split$cv.group==i,3]<-haz.pred.all[cvtest.rows,3][cvtest$tstart<cvtest$time]
+  surv.pred[dta_train$cv.group==i,3]<-ave(1-haz.pred.all[cvtest.rows,3],cvtest$pid,FUN=cumprod)[cvtest$tstart==10]
   
   #---
   #method 4: random forest
   method.rf <- ranger(status ~ tstart+year1+year2+age+meno+size1+size2+grade+nodes+pgr+er+hormon+chemo,
-                      data=train,oob.error = FALSE)
-  haz.pred.all[test.rows,4] <- predict(method.rf, data = test)$predictions
-  haz.pred[dta.split$cv.group==i,4]<-haz.pred.all[test.rows,4][test$tstart<test$time]
-  surv.pred[dta_train$cv.group==i,4]<-ave(1-haz.pred.all[test.rows,4],test$pid,FUN=cumprod)[test$tstart==10]
+                      data=cvtrain,oob.error = FALSE)
+  haz.pred.all[cvtest.rows,4] <- predict(method.rf, data = cvtest)$predictions
+  haz.pred[dta_train_split$cv.group==i,4]<-haz.pred.all[cvtest.rows,4][cvtest$tstart<cvtest$time]
+  surv.pred[dta_train$cv.group==i,4]<-ave(1-haz.pred.all[cvtest.rows,4],cvtest$pid,FUN=cumprod)[cvtest$tstart==10]
   
   #-------------------------------------------------------
   #Calculate expected loss for each method
@@ -102,20 +105,27 @@ for(i in 1:n.fold){
   #-------------------------------------------------------
   
   #estimate censoring survival distribution (assuming independent censoring), which is needed as a weight in the ipcw loss function
-  cens.km<-survfit(Surv(time,1-status)~1,data=test.1row)
-  cens.wt<-1/summary(cens.km,times = 10)$surv
+  cens.km<-survfit(Surv(time,1-status)~1,data=cvtest.1row)
+  # cens.wt[i]<-1/summary(cens.km,times = 10)$surv
+  
+  cens.km.step<-stepfun(cens.km$time,c(1,cens.km$surv))
+  cens.wt[cvtest.rows.1row]<-1/cens.km.step(pmin(cvtest.1row$time,10))
   
   for(j in 1:n.methods){
     #L2 loss
-    loss.L2[i,j]<-sum((test.split$status-haz.pred[dta.split$cv.group==i,j])^2)
+    loss.L2[i,j]<-mean((cvtest.split$status-haz.pred[dta_train_split$cv.group==i,j])^2)
     
     #loglik loss
-    loss.loglik[i,j]<-sum((log(haz.pred[dta.split$cv.group==i,j])^test.split$status)*
-                        (log(1-haz.pred[dta.split$cv.group==i,j])^(1-test.split$status)))
+    loss.loglik[i,j]<-mean((log(haz.pred[dta_train_split$cv.group==i,j])^cvtest.split$status)*
+                        (log(1-haz.pred[dta_train_split$cv.group==i,j])^(1-cvtest.split$status)))
+    
+    # #ipcw loss
+    # loss.ipcw[i,j]<-mean(cens.wt[i]*I(cvtest.1row$status==1|(cvtest.1row$status==0 & cvtest.1row$time>10))*
+    #                       (I(cvtest.1row$time>10)-surv.pred[dta_train$cv.group==i,j])^2)
     
     #ipcw loss
-    loss.ipcw[i,j]<-sum(cens.wt*I(test.1row$status==1|(test.1row$status==0 & test.1row$time>10))*
-                          (I(test.1row$time>10)-surv.pred[dta_train$cv.group==i,j])^2)
+    loss.ipcw[i,j]<-mean(cens.wt[cvtest.rows.1row]*I(cvtest.1row$status==1|(cvtest.1row$status==0 & cvtest.1row$time>10))*
+                           (I(cvtest.1row$time>10)-surv.pred[dta_train$cv.group==i,j])^2)
   }
 }
 
@@ -141,7 +151,7 @@ discrete.SL
 
 #---
 #ensemble based on L2 loss function, using non-negative least squares
-comb.mod.L2<-nnls(b=dta.split$status,A=haz.pred)
+comb.mod.L2<-nnls(b=dta_train_split$status,A=haz.pred)
 
 #scale the coefficients to sum to 1
 weights.L2<-comb.mod.L2$x/sum(comb.mod.L2$x)
@@ -153,20 +163,23 @@ weights.L2<-comb.mod.L2$x/sum(comb.mod.L2$x)
 g.haz.loglik<-log(haz.pred/(1-haz.pred))
 
 #fit combining model, without an intercept
-comb.mod.loglik<-glm(dta.split$status~-1+g.haz.loglik,family="binomial")
+comb.mod.loglik<-glm(dta_train_split$status~-1+g.haz.loglik,family="binomial")
 
 #no need to scale the coefficients for this loss function
 weights.loglik<-comb.mod.loglik$coefficients
 
 #---
 #ensemble based on ipcw loss function, using non-negative weighted least squares
-#This is achieved by multiplying the outcome and explnatory variables by the square root of the weights. 
+#This is achieved by multiplying the outcome and explanatory variables by the square root of the weights. 
 #https://stackoverflow.com/questions/47888996/weighted-nonnegative-least-squares-in-r
-cens.wt.dta<-data.frame(cens.wt=cens.wt,cv.group=1:10)
-dta_train2<-merge(dta_train,cens.wt.dta)
+# cens.wt.dta<-data.frame(cens.wt=cens.wt,cv.group=1:n.fold)
+# dta_train2<-merge(dta_train,cens.wt.dta)
+
+dta_train2<-dta_train
+dta_train2$cens.wt<-cens.wt
+
 dta_train2<-dta_train2[order(dta_train2$pid),]
 dta_train2$elig<-I(dta_train2$status==1|(dta_train2$status==0 & dta_train2$time>10))
-dta_train2$status<-ifelse(dta_train2$status==1 & dta_train2$time<=10,1,0)
 comb.mod.ipcw<-nnls(b=I(dta_train2$status[dta_train2$elig==1]==0)*sqrt(dta_train2$cens.wt[dta_train2$elig==1]),
                     A=surv.pred[dta_train2$elig==1,]*sqrt(dta_train2$cens.wt[dta_train2$elig==1]))
 
@@ -195,15 +208,15 @@ ensemble.SL
 
 #---
 #generate test data in the long format needed to obtain predictions
-dta.haz.test<-merge(dta_test,times)
-dta.haz.test<-dta.haz.test[order(dta.haz.test$pid,dta.haz.test$tstart),]
-dta.haz.test$tstart2<-dta.haz.test$tstart^2
-dta.haz.test$tstart3<-dta.haz.test$tstart^3
+dta_test_haz<-merge(dta_test,times)
+dta_test_haz<-dta_test_haz[order(dta_test_haz$pid,dta_test_haz$tstart),]
+dta_test_haz$tstart2<-dta_test_haz$tstart^2
+dta_test_haz$tstart3<-dta_test_haz$tstart^3
 
 #---
 #storing predicted hazards from each method
 n.methods=4
-haz.pred.all<-matrix(nrow=dim(dta.haz.test)[1],ncol=n.methods)
+haz.pred.all<-matrix(nrow=dim(dta_test_haz)[1],ncol=n.methods)
 
 #storing predicted survival probabilities from each method
 n.test<-dim(dta_test)[1]
@@ -211,32 +224,32 @@ surv.pred<-matrix(nrow=n.test,ncol=n.methods)
 
 #---
 #method 1: glm with no covariates
-method.glm0<-glm(status~1,data=dta.split,family = "binomial")
-haz.pred.all[,1]<-predict(method.glm0,newdata=dta.haz.test,type="response")
-surv.pred[,1]<-ave(1-haz.pred.all[,1],dta.haz.test$pid,FUN=cumprod)[dta.haz.test$tstart==10]
+method.glm0<-glm(status~1,data=dta_train_split,family = "binomial")
+haz.pred.all[,1]<-predict(method.glm0,newdata=dta_test_haz,type="response")
+surv.pred[,1]<-ave(1-haz.pred.all[,1],dta_test_haz$pid,FUN=cumprod)[dta_test_haz$tstart==10]
 
 #---
 #method 2: glm including time plus covariates
 method.glm1<-glm(status~tstart+tstart2+tstart3+year1+year2+age+meno+size1+size2+grade+nodes+pgr+er+hormon+chemo,
-                 data=dta.split,family = "binomial")
-haz.pred.all[,2]<-predict(method.glm0,newdata=dta.haz.test,type="response")
-surv.pred[,2]<-ave(1-haz.pred.all[,2],dta.haz.test$pid,FUN=cumprod)[dta.haz.test$tstart==10]
+                 data=dta_train_split,family = "binomial")
+haz.pred.all[,2]<-predict(method.glm1,newdata=dta_test_haz,type="response")
+surv.pred[,2]<-ave(1-haz.pred.all[,2],dta_test_haz$pid,FUN=cumprod)[dta_test_haz$tstart==10]
 
 #---
 #method 3: lasso
 xmat.train<-model.matrix(method.glm1)[,-1]
-dta.haz.test$status=99#just a trick so that we can use model.matrix to get xmat.test
-xmat.test<-model.matrix(method.glm1,data=dta.haz.test)[,-1]
-method.lasso <- cv.glmnet(x=xmat.train,y=dta.split$status,family = "binomial",alpha=1)
+dta_test_haz$status=99#just a trick so that we can use model.matrix to get xmat.test
+xmat.test<-model.matrix(method.glm1,data=dta_test_haz)[,-1]
+method.lasso <- cv.glmnet(x=xmat.train,y=dta_train_split$status,family = "binomial",alpha=1)
 haz.pred.all[,3]<-predict(method.lasso, newx = xmat.test, s = "lambda.min",type="response")
-surv.pred[,3]<-ave(1-haz.pred.all[,3],dta.haz.test$pid,FUN=cumprod)[dta.haz.test$tstart==10]
+surv.pred[,3]<-ave(1-haz.pred.all[,3],dta_test_haz$pid,FUN=cumprod)[dta_test_haz$tstart==10]
 
 #---
 #method 4: random forest
 method.rf <- ranger(status ~ tstart+year1+year2+age+meno+size1+size2+grade+nodes+pgr+er+hormon+chemo,
-                    data=dta.split,oob.error = FALSE)
-haz.pred.all[,4] <- predict(method.rf, data = dta.haz.test)$predictions
-surv.pred[,4]<-ave(1-haz.pred.all[,4],dta.haz.test$pid,FUN=cumprod)[dta.haz.test$tstart==10]
+                    data=dta_train_split,oob.error = FALSE)
+haz.pred.all[,4] <- predict(method.rf, data = dta_test_haz)$predictions
+surv.pred[,4]<-ave(1-haz.pred.all[,4],dta_test_haz$pid,FUN=cumprod)[dta_test_haz$tstart==10]
 
 #---
 #now combine using the ensemble weights
@@ -247,10 +260,12 @@ surv.pred[,4]<-ave(1-haz.pred.all[,4],dta.haz.test$pid,FUN=cumprod)[dta.haz.test
 haz.pred.SL.L2<-weights.L2[1]*haz.pred.all[,1]+weights.L2[2]*haz.pred.all[,2]+
   weights.L2[3]*haz.pred.all[,3]+weights.L2[4]*haz.pred.all[,4]
 
-surv.pred.SL.L2<-ave(1-haz.pred.SL.L2,dta.haz.test$pid,FUN=cumprod)[dta.haz.test$tstart==10]
+surv.pred.SL.L2<-ave(1-haz.pred.SL.L2,dta_test_haz$pid,FUN=cumprod)[dta_test_haz$tstart==10]
 
 #---
 #ensemble SL based on loglik loss function
+
+#obtain logit transformations of the estimated hazards
 logit<-function(x){log(x/(1-x))}
 expit<-function(x){exp(x)/(1+exp(x))}
 
@@ -262,7 +277,7 @@ g.haz.pred.SL.loglik<-
 
 haz.pred.SL.loglik<-expit(g.haz.pred.SL.loglik)
 
-surv.pred.SL.loglik<-ave(1-haz.pred.SL.loglik,dta.haz.test$pid,FUN=cumprod)[dta.haz.test$tstart==10]
+surv.pred.SL.loglik<-ave(1-haz.pred.SL.loglik,dta_test_haz$pid,FUN=cumprod)[dta_test_haz$tstart==10]
 
 #---
 #ensemble SL based on ipcw
@@ -279,7 +294,7 @@ surv.pred.SL.ipcw<-weights.ipcw[1]*surv.pred[,1]+
 #-------------------------------------------------------
 
 #the line below can be altered depending on which SL we want to evaluate (surv.pred.SL.L2,surv.pred.SL.loglik,surv.pred.SL.ipcw)
-risk.pred<-1-surv.pred.SL.loglik
+risk.pred<-1-surv.pred.SL.ipcw
 
 #---------------------------------
 #Calibration plots
